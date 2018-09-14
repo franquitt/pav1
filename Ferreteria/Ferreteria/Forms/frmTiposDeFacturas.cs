c06267a7c812a3b27e0cbd6a5cf5daad67cb8151
Agus Venturi <agus97v@gmail.com>
2018-09-13 21:04:03 -0300

    Agregado ABMC tipo de facturas (falta testeo)

--- Ferreteria/Ferreteria/Forms/frmTiposDeFacturas.cs
+++ Ferreteria/Ferreteria/Forms/frmTiposDeFacturas.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ferreteria.Models;
 
 namespace Ferreteria.Forms
 {
@@ -17,24 +18,44 @@ namespace Ferreteria.Forms
             InitializeComponent();
         }
 
-        private void frmTipoFacturas_Load(object sender, EventArgs e)
+        //Carga los tipos de factura existentes en la grilla
+        public void frmTipoFacturas_Load(object sender, EventArgs e)
         {
-
+            gridTipoFacturas.DataSource = TipoFactura.GetAllTipoFacturas();
         }
 
+        //Abre el formulario para agregar un nuevo tipo de factura
         private void btnAddTipoFactura_Click(object sender, EventArgs e)
         {
 
         }
 
+        //Abre el formulario para editar el tipo de factura seleccionado
         private void btnEditTipoFactura_Click(object sender, EventArgs e)
         {
 
         }
 
+        //Permite dar de baja el tipo de factura elegido
         private void btnRemoveTipoFactura_Click(object sender, EventArgs e)
         {
-
+            int id = Helper.getSelectedId(gridTipoFacturas);
+            try
+            {
+                TipoFactura tipoFactura = new TipoFactura(id);
+                var confirmResult = MessageBox.Show("Esta seguro que desea deshabilitar el tipo de factura " + tipoFactura.nombre + " ?",
+                                         "Dar de baja!",
+                                         MessageBoxButtons.YesNo);
+                if (confirmResult == DialogResult.Yes)
+                {
+                    tipoFactura.available(false);
+                    frmTipoFacturas_Load(null, null);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Se produjo un error, asegurese de seleccionar toda la fila.", "Error", MessageBoxButtons.OK);
+            }
         }
     }
 }
---
+++ Ferreteria/Ferreteria/Models/TipoFactura.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Ferreteria.Models
+{
+    class TipoFactura
+    {
+        public int codigoTipo = 0;
+        public string nombre = "";
+        public bool activo = true;
+
+
+        //Constructor nuevo tipo factura
+        public TipoFactura(int id, string nombre)
+        {
+            codigoTipo = id;
+            this.nombre = nombre;
+        }
+
+        //Constructor tipo factura existente
+        public TipoFactura(int id)
+        {
+            codigoTipo = id;
+            readAttrs();
+        }
+
+        //Metodo que busca los datos de un tipo de factura
+        private void readAttrs()
+        {
+            DataTable values = BDHelper.ConsultaSQL("SELECT nombre, activo FROM TIPO_FACTURA WHERE codigoTipo = " + codigoTipo);
+            nombre = values.Rows[0]["nombre"].ToString();
+            activo = (bool)values.Rows[0]["activo"];
+        }
+
+        //Metodo que guarda los datos de un tipo de factura nuevo o uno ya existente
+        public bool save()
+        {
+            try
+            {
+                if (codigoTipo != 0)//Tipo de factura existente
+                {
+                    BDHelper.ExcecuteSQL("UPDATE TIPO_FACTURA SET nombre = '" + nombre + "', activo = " + getActivo() + " WHERE codigoTipo = " + codigoTipo);
+                }
+                else//Tipo de factura nuevo
+                {
+                    BDHelper.ExcecuteSQL("INSERT INTO TIPO_FACTURA( nombre, activo) VALUES('" + nombre + "', " + getActivo() + ")");
+                }
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Devuelve si el tipo de factura esta activo en el sistema y puede usarse
+        private string getActivo()
+        {
+            if (activo)
+                return "1";
+            return "0";
+        }
+
+        //Modifica el estado del tipo de factura a activo o inactivo según corresponda
+        public void available(bool state)
+        {
+            activo = state;
+            BDHelper.ExcecuteSQL("UPDATE TIPO_FACTURA SET activo = " + getActivo() + " WHERE codigoTipo = " + codigoTipo);
+        }
+
+        //Devuelve una tabla con todos los tipos de facturas activas del sistema
+        public static DataTable GetAllTipoFacturas()
+        {
+            return BDHelper.ConsultaSQL("SELECT codigoTipo, nombre FROM TIPO_FACTURA WHERE activo = 1 ORDER BY nombre ASC");
+        }
+    }
+}
