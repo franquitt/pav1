e43916e29bb2bbc70b1653b74d737d0fbbf1610b
FrancoMain <franquitt@gmail.com>
2018-10-10 00:24:07 -0300

    agregada clase lote complete?

--- Ferreteria/Ferreteria/Models/Lote.cs
+++ Ferreteria/Ferreteria/Models/Lote.cs
@@ -20,6 +20,17 @@ namespace Ferreteria.Models
             this.nroLote = nroLote;
             readAttrs();
         }
+        public Lote()
+        {
+        }
+
+        public void save()
+        {
+            if (nroLote == 0)
+                BDHelper.ExcecuteSQL("INSERT INTO LOTES(codigoProveedor, codigoProducto, stockInicial, stockActual, fechaIngreso) VALUES("+codigoProveedor+", "+codigoProducto+", "+stockInicial+", "+stockActual+", '"+fechaIngreso+"')");
+            else
+                BDHelper.ExcecuteSQL("UPDATE LOTES SET codigoProveedor = " + codigoProveedor + ", codigoProducto = " + codigoProducto + ", stockInicial = " + stockInicial + ", stockActual = " + stockActual + ", fechaIngreso = '" + fechaIngreso + "' WHERE numeroLote = "+ nroLote+"");
+        }
         public void readAttrs()
         {
             DataTable values = BDHelper.ConsultaSQL("SELECT codigoProveedor, codigoProducto, stockInicial, stockActual, fechaIngreso FROM LOTES WHERE numeroLote = " + nroLote);
@@ -30,7 +41,24 @@ namespace Ferreteria.Models
             fechaIngreso = DateTime.Parse(values.Rows[0]["fechaIngreso"].ToString());
         }
         public static DataTable getAllLotes() {
-            return BDHelper.ConsultaSQL("SELECT LOTES.numeroLote, PROVEEDOR.nombre AS nomProveedor, PRODUCTOS.nombre AS nomProducto, LOTES.stockActual, LOTES.fechaIngreso FROM LOTES JOIN PROVEEDOR ON(PROVEEDOR.codigoProveedor = LOTES.codigoProveedor) JOIN PRODUCTOS ON(PRODUCTOS.codigoProducto = LOTES.codigoProducto)");
+            return BDHelper.ConsultaSQL("SELECT LOTES.numeroLote, PROVEEDOR.nombre AS nomProveedor, PRODUCTOS.nombre AS nomProducto, LOTES.stockActual, LOTES.fechaIngreso FROM LOTES JOIN PROVEEDOR ON(PROVEEDOR.codigoProveedor = LOTES.codigoProveedor) JOIN PRODUCTOS ON(PRODUCTOS.codigoProducto = LOTES.codigoProducto) ORDER BY LOTES.fechaIngreso");
+        }
+        public static Lote[] getAllLotesObjectsByProduct(int codProd)
+        {
+            DataTable values =  BDHelper.ConsultaSQL("SELECT numeroLote, codigoProveedor, codigoProducto, stockInicial, stockActual, fechaIngreso FROM LOTES WHERE codigoProducto = "+ codProd + " ORDER BY fechaIngreso ASC");
+            Lote[] lotes = new Lote[values.Rows.Count];
+            for(int index = 0;index < values.Rows.Count; index++)
+            {
+                lotes[index] = new Lote();
+                lotes[index].nroLote = (int)values.Rows[index]["numeroLote"];
+                lotes[index].codigoProveedor = (int)values.Rows[index]["codigoProveedor"];
+                lotes[index].codigoProducto = (int)values.Rows[index]["codigoProducto"];
+                lotes[index].stockInicial = (int)values.Rows[index]["stockInicial"];
+                lotes[index].stockActual = (int)values.Rows[index]["stockActual"];
+                lotes[index].fechaIngreso = DateTime.Parse(values.Rows[index]["fechaIngreso"].ToString());
+
+            }
+            return lotes;
         }
     }
 }
